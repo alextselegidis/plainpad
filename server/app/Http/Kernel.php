da90f89862ecbf3a79dc4191bc8a611c574e3dcf
Alex Tselegidis <alextselegidis@gmail.com>
2020-09-25 13:28:40 +0300

    Added the admin middleware registration

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'admin' => \App\Http\Middleware\Admin::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
