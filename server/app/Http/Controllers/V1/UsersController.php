7bfa2cc8dce9a5b8d777859ce9da4a88c562247e
Alex Tselegidis <alextselegidis@gmail.com>
2021-01-07 09:45:35 +0200

    Removed unnecessary import

--- server/app/Http/Controllers/V1/UsersController.php
+++ server/app/Http/Controllers/V1/UsersController.php
@@ -28,7 +28,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
