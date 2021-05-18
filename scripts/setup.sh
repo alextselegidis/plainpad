667da6b0039c903a953560d4a6b9c3c15b8dafe5
Sazzad Hossain Sharkar <sh@sharkar.net>
2021-05-19 02:33:38 +0600

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,17 +2,18 @@
 
 echo "Setup Server"
 cd server
-composer install
-cd ../../
+composer install && composer update
+cd ..
 
 echo "Setup Client"
 cd client
 cp .env.example .env
-npm install
-cd ../../
+npm install && npm update
 
 echo "Create .env File"
 cp .env.example .env
+
+cd ..
 
 echo "Change Storage Permissions"
 chmod -R 777 server/storage
