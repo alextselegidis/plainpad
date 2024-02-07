5a1da3a353d7273b6cca7088eaf67158a882e932
Tianjiu <84212214+T1anjiu@users.noreply.github.com>
2024-02-07 21:26:51 +0800

    Add files via upload

---
+++ server/resources/lang/zh-CN/account.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+  Plainpad - Self Hosted Note Taking App
+
+  Copyright (C) 2020 Alex Tselegidis - https://alextselegidis.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+return [
+    'registration' => '注册',
+    'registrationMessage' => '感谢您在 :url 上创建帐户，请使用您的凭据登录。',
+    'resetPassword' => '重置您的密码',
+    'resetPasswordMessage' => '使用以下密码登录您的帐户，请尽快登录后更改您的密码。',
+    'resetPasswordPreheader' => '此邮件包含您的新密码。请使用它登录并为您的帐户设置一个新密码。',
+];
