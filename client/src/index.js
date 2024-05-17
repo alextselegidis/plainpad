161703b5ebccc4b0b7336f70d334bf1001aca9e7
Alex Tselegidis <alextselegidis@gmail.com>
2024-05-17 17:50:57 +0200

    Fix the service worker generation problem

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {Provider} from 'mobx-react';
 import {IntlProvider} from 'react-intl';
 import {messages} from './lang';
@@ -31,4 +31,4 @@ ReactDOM.render(<Root/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register({});
+serviceWorkerRegistration.unregister();
