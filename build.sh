338d99d3c74dce00a02990fb62c0aea11920444e
Alex Tselegidis <alextselegidis@gmail.com>
2024-12-13 17:22:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Reset Build Directory"
+
 rm -rf build/ build.zip
 mkdir build/
 
 echo "Bundle Server"
+
 cp -r server/** build/
 cp server/.env.example build/
 rm server/storage/**/*.log
@@ -19,30 +21,41 @@ rm composer.lock
 cd ../
 
 echo "Bundle Client"
+
 cd client/
 npm run build
 cd ../
 cp -r client/build/** build/public
 
 echo "Copy README File"
+
 cp README.md build/
 
-echo "Zip Build"
+echo "Create Zip"
+
 cd build
+
 find . -name '.DS_Store' -type f -delete
-zip -r build.zip . \
-    -x .git/**\* \
-    -x .idea/**\* \
-    -x tests/**\* \
-    -x .editorconfig \
-    -x .gitattributes \
-    -x .phpstorm.meta.php \
-    -x .prettierignore \
-    -x package-lock.json \
-    -x phpunit.xml \
-    -x _ide_helper.php \
-    -x vite.config.js \
-    -x \*.zip
+
+zip -r build.zip . -x \
+    '.idea/*' \
+    '.git/*' \
+    '.github/*' \
+    '.run/*' \
+    'docker/*' \
+    'tests/*' \
+    '.editorconfig' \
+    '.gitattributes' \
+    '.gitignore' \
+    '.phpstorm.meta.php' \
+    '.prettierignore' \
+    '.prettierrc.json' \
+    'package-lock.json' \
+    'phpunit.xml' \
+    '_ide_helper_models.php' \
+    '_ide_helper.php' \
+    'build.sh' \
+    '*.zip'
 
 cd ..
 
